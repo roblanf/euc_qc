c1f3d815ca4cc6e3e367109a9995dd819f2eb3d3
roblanf <rob.lanfear@gmail.com>
2016-12-20 12:27:10 +1100

    fix logging in bbduk

--- qc.sh
+++ qc.sh
@@ -53,7 +53,7 @@ for in1 in $(find $inputf -name "*R1_001.fastq.gz"); do
     out1=$outputtrimreads$f1
     out2=$outputtrimreads$f2
 
-    sampleid=$outputtrimreads$(basename ${in1%%R1_001.fastq.gz}"")
+    sampleid=$outputtrimreads${f1%%R1_001_trimmed.fastq.gz}
 
 
     echo "writing output to"
@@ -61,9 +61,7 @@ for in1 in $(find $inputf -name "*R1_001.fastq.gz"); do
     echo $out2
 
     # run bbduk and save ALL the output files    
-    $bbduk in1=$in1 in2=$in2 out1=$out1 out2=$out2 minlen=$minlen k=25 mink=8 ktrim=r ref=$adaptors hdist=1 overwrite=f qtrim=rl trimq=$trimq t=$threads bhist=$sampleid"bhist.txt" qhist=$sampleid"qhist.txt" gchist=$sampleid"gchist.txt" aqhist=$sampleid"aqhist.txt" lhist=$sampleid"lhist.txt" > $sampleid
-
-
+    $bbduk in1=$in1 in2=$in2 out1=$out1 out2=$out2 minlen=$minlen k=25 mink=8 ktrim=r ref=$adaptors hdist=1 overwrite=f qtrim=rl trimq=$trimq t=$threads bhist=$sampleid"bhist.txt" qhist=$sampleid"qhist.txt" gchist=$sampleid"gchist.txt" aqhist=$sampleid"aqhist.txt" lhist=$sampleid"lhist.txt" > $sampleid"bbduk_log.txt"
 
 done
 
