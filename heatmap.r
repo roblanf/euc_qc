bc42e3eb80d3696bcfd9c74200b73b98de80c658
roblanf <rob.lanfear@gmail.com>
2017-03-30 11:29:04 +1100

    draw a heat map of the MASH distances
    note that the strsplit things may not (!!) work for most peoples
    samples, but that’s easy to change.

---
+++ heatmap.r
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+inputf = args[1]
+outputf = args[2]
+
+library(reshape2)
+
+d = read.delim(inputf, header=F)
+d = d[,c(1,2,3)]
+names(d) = c("sample1", "sample2", "distance")
+
+dst = acast(d, sample1 ~ sample2)
+dst = data.matrix(dst)
+dim = ncol(dist)
+
+samples = rownames(dst)
+splits = strsplit(samples, "_")
+splitmat = matrix(unlist(splits),ncol=3,byrow=TRUE)
+samples = as.character(as.data.frame(splitmat)[,2])
+
+rownames(dst) = samples
+colnames(dst) = samples
+
+pdf(file=outputf)
+
+image(1:dim, 1:dim, dst, axes = FALSE, xlab="", ylab="")
+axis(1, 1:dim, rownames(dst), cex.axis = 0.5, las=3)
+axis(2, 1:dim, rownames(dst), cex.axis = 0.5, las=1)
+text(expand.grid(1:dim, 1:dim), sprintf("%.3f", dst), cex=0.4)
+
+dev.off()
