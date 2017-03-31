1a651ec98021e7f51ea6f8165916a3a288a1f4c8
roblanf <rob.lanfear@gmail.com>
2017-03-31 23:16:34 +1100

    updates to genome scope script
    it took me an inordinately long time to figure some of that out. But it
    runs nicely now.

--- genomescope.sh
+++ genomescope.sh
@@ -1,4 +1,4 @@
-inputf="/disks/dacelo/data/raw_data/tree_EM1/Project_SN7001117R_0083_CKulheim_LBronham_Melaleuca/Sample_M1a_index2/"
+inputf="/disks/dacelo/data/raw_data/tree_EM1/Project_SN7001117R_0083_CKulheim_LBronham_Melaleuca/"
 outputf="/disks/dacelo/data/QC/Project_SN7001117R_0083_CKulheim_LBronham_Melaleuca/genomescope/"
 
 threads=20 # how many threads do you want to use?
@@ -18,9 +18,32 @@ for in1 in $(find $inputf -name "*R1_001.fastq.gz"); do
     readf=$id".fastq.gz"
     cat $in1 $in2 > $readf
 
-	jellyfish count -C -m $kmer_size -s $s -t $threads $readf -o $outputf$id'reads.jf'
-	jellyfish histo -t $threads $id'reads.jf' > $id'reads.histo'
+done
 
-	Rscript genomescope.R $id'reads.histo' $kmer_size $read_length $outputf$id $kmer_max verbose
+cat Sample_M1* > M1.fastq.gz
+cat Sample_M2* > M2.fastq.gz
+cat Sample_M3* > M3.fastq.gz
+cat Sample_M4* > M4.fastq.gz
+cat Sample_M5* > M5.fastq.gz
+cat Sample_M6* > M6.fastq.gz
+cat Sample_M7* > M7.fastq.gz
+cat Sample_M8* > M8.fastq.gz
+
+rm Sample*
+
+# make one fastq with ALL THE READS (since each sample is only ~30x which is potentially a little low)
+cat *.fastq.gz > all.fastq.gz
+
+for readf in $(find *.fastq.gz); do
+
+	zcat $readf | jellyfish count -C -m $kmer_size -s $s -t $threads -o $outputf$readf'.jf' /dev/fd/0
+	jellyfish histo -t $threads $outputf$readf'.jf' > $readf'.histo'
+
+	# run genomescope and put the output in a new directory
+	id=`echo "$readf" | cut -d'.' -f1`
+	genomescope.R $readf'.histo' $kmer_size $read_length $outputf$id $kmer_max verbose
 
 done
+
+rm *.fastq.gz
+rm *.jf
