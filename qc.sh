9cc8e13ac9b6ebd0aba599966bf03bcac427022a
roblanf <rob.lanfear@gmail.com>
2017-04-27 08:33:16 +1000

    vigorous code cleaning

--- qc.sh
+++ qc.sh
@@ -9,38 +9,25 @@ outputbase="/disks/dacelo/data/QC/test/"
 ref="/disks/dacelo/data/raw_data/active_refs/Emel.fa.gz" # reference file as a fasta
 gff="/disks/dacelo/data/raw_data/active_refs/Egrandis_genes_chr1_to_chr11.gff3"
 adaptors="/disks/dacelo/data/programs/bbmap/resources/adapters.fa"
-
-# for more control of the parameters, you can change them in the commandlines below
-# of course, if there's a parameter you really want to explore, make it a variable here.
-
 threads=50 # number of threads to use
 minlen=50 # minimum length of read to keep after trimming
-trimq=20 # trim bases with quality < this 
+trimq=0 # trim bases with quality < this 
 
+
+# set up dirs
 outputrawqc=$outputbase"rawqc/"
 outputtrimqc=$outputbase"trimmedqc/"
 outputtrimreads=$outputbase"trimmed_reads/"
+ngmout=$outputbase"ngm/"    
 indexcov=$outputbase"indexcov/"
-
-echo $outputbase
-echo $outputrawqc
-echo $outputtrimqc
-echo $outputtrimreads
-echo $indexcov
-
-# set up dirs
 mkdir $outputbase
-mkdir $outputbase"ngm"
+mkdir $ngmout
 mkdir $outputrawqc
 mkdir $outputtrimqc
 mkdir $outputtrimreads
 
 # run bbduk on all pairs of samples
-# sample pairs look like:
-# RL41_S1_R1_001.fastq.gz
-# RL41_S1_R2_001.fastq.gz
 echo "Trimming with bbduk"
-date
 
 for in1 in $(find $inputf -name "*R1_001.fastq.gz"); do
     in2=${in1%%R1_001.fastq.gz}"R2_001.fastq.gz"
@@ -56,92 +43,49 @@ for in1 in $(find $inputf -name "*R1_001.fastq.gz"); do
 
     sampleid=$outputtrimreads${f1%%R1_001_trimmed.fastq.gz}
 
-
-    echo "writing output to"
-    echo $out1
-    echo $out2
-
-    # run bbduk and save ALL the output files    
     bbduk.sh in1=$in1 in2=$in2 out1=$out1 out2=$out2 minlen=$minlen k=25 mink=8 ktrim=r ref=$adaptors hdist=1 overwrite=f qtrim=rl trimq=$trimq t=$threads bhist=$sampleid"bhist.txt" qhist=$sampleid"qhist.txt" gchist=$sampleid"gchist.txt" aqhist=$sampleid"aqhist.txt" lhist=$sampleid"lhist.txt" > $sampleid"bbduk_log.txt"
 
 done
 
-echo "Done trimming"
-date
-
 # run fastqc on all the raw and trimmed data files
 echo "Running fastqc"
-date
-#find $inputf -name '*.fastq.gz' | xargs fastqc  -o $outputrawqc -t $threads
-#find $outputtrimreads -name '*.fastq.gz' | xargs fastqc  -o $outputtrimqc -t $threads
-
-echo "Done running fastqc"
-date
+find $inputf -name '*.fastq.gz' | xargs fastqc  -o $outputrawqc -t $threads
+find $outputtrimreads -name '*.fastq.gz' | xargs fastqc  -o $outputtrimqc -t $threads
 
 # map reads to E. grandis reference
 # our trimmed files look like: RL41_S1_R1_001_trimmed.fastq.gz  RL41_S1_R2_001_trimmed.fastq.gz
-echo "Mapping"
-date
+echo "Mapping to reference with NGM"
 
 for in1 in $(find $outputtrimreads -name "*R1_001_trimmed.fastq.gz"); do
     in2=${in1%%R1_001_trimmed.fastq.gz}"R2_001_trimmed.fastq.gz"
-    echo ""
-    echo "mapping with NextGenMap"
+    echo "mapping files: "
     echo $in1
     echo $in2
 
+    # output file setup
     f1=$(basename $in1)
-
-    ngmout=$outputbase"ngm/"
-    
     id=${f1%%R1_001_trimmed.fastq.gz}
-
-    stats=$ngmout"stats_"$id"trimmed.txt"
-    covhist=$ngmout"covhist_"$id"trimmed.txt"
-    ihist=$ngmout"ihist_"$id"trimmed.txt"
     outsamngm=$ngmout$id"trimmed.sam"
 
-    echo "running ngm"
-    echo $ngmout
     ngm -t $threads -p -r $ref -1 $in1 -2 $in2 -o $outsamngm
-
-    echo "converting sam to bam"
-    outbamngm=$ngmout$id"trimmed"
-    samtools view -bS -@ $threads $outsamngm > $outbamngm".bam"
-
-    echo "sorting bam"
-    samtools sort -@ $threads  $outbamngm".bam" -o $outbamngm".bam"
-
-    echo "deleting sams and trimmed reads"
+    outbamngm=$ngmout$id"trimmed.bam"
+    samtools view -bS -@ $threads $outsamngm > $outbamngm
+    samtools sort -@ $threads  $outbamngm -o $outbamngm
     rm $outsamngm
-    rm $in1
-    rm $in2
     
     echo "running qualimap on sorted bams"
     outqualimap_all=$ngmout$id"qualimap_all/"
-    qualimap bamqc -bam $outbamngm".bam" -outdir $outqualimap_all -nt $threads -c
+    qualimap bamqc -bam $outbamngm -outdir $outqualimap_all -nt $threads -c
     outqualimap_gff=$ngmout$id"qualimap_gff/"
-	qualimap bamqc -bam $outbamngm".bam" -outdir $outqualimap_gff -gff $gff -nt $threads -c
+	qualimap bamqc -bam $outbamngm -outdir $outqualimap_gff -gff $gff -nt $threads -c
 
 done
-echo "Done Mapping"
-date
 
 echo "indexing bams"
-date
 ls ${ngmout}*.bam | parallel "samtools index {}"
-echo "done indexing bams"
-date
 
 echo "running indexcov"
-date
 goleft indexcov --directory $indexcov --sex "" ${ngmout}"*.bam"
-echo "done running indexcov"
-date
 
 echo "running multiqc"
-date
 multiqc $outputbase -o $outputbase
-date
-echo "done running multiqc"
-date
